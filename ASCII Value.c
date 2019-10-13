dd77369d8b302dfa1934c41123130fa4f16d4341
808047 <49825761+808047@users.noreply.github.com>
2019-10-13 21:09:20 +0530

    Print ASCII Value of Character

---
+++ ASCII Value.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    //To print ASCII value of character....
+    char c;
+    printf("Enter a character: ");
+    scanf("%c", &c);  
+    printf("ASCII value of %c = %d", c, c);
+    return 0;
+}
