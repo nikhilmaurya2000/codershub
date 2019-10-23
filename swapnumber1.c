476d71278f352a1dceeac0ef1200eff938dd6a0d
belikeamitesh <amitesh46237242@gmail.com>
2019-10-23 12:04:50 +0530

    Very bad program for swapping two numbers

---
+++ swapnumber1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("Enter two numbers : ");
+	scanf("%d%d",&a,&b);
+	printf("The numbers are swapped %d and %d",b,a);
+	return 0;
+	//lol  ye to mera hai program
+}
